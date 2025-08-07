41007161fd7257e2b980ee071f045f5184e91bbb
Cruz, Thierry S <33569708+thierryscruz@users.noreply.github.com>
2025-08-07 13:32:02 -0300

    Add files via upload

---
+++ Convmin.sql
@@ -0,0 +1,50 @@
+CREATE FUNCTION [FORPONTO].[CONVMIN_SALDO] (@MINUTOS int)
+RETURNS NVARCHAR(30)
+BEGIN
+   DECLARE @iHoras   INTEGER
+   DECLARE @iMinutos INTEGER 
+   DECLARE @sEdita   VARCHAR(30)
+   
+   SET @iHoras = CAST(ROUND(@MINUTOS/60, 0) AS INT)
+   SET @iMinutos = @MINUTOS % 60     
+	
+	 IF @MINUTOS < 0
+		BEGIN
+		SET @iHoras = CAST(ROUND((@MINUTOS * -1)/60, 0) AS INT)
+		SET @iMinutos = (@MINUTOS * -1) % 60   
+			BEGIN
+				SET @sEdita = CASE LEN(@iHoras)
+								WHEN 0 THEN '00'
+								WHEN 1 THEN '0' + CONVERT(NVARCHAR(1), @iHoras)
+								ELSE CONVERT(NVARCHAR(8),@iHoras)
+								END
+	
+				SET @sEdita = '-' + @sEdita + ':' + CASE LEN(@iMinutos)
+												WHEN 0 THEN '00' 
+												WHEN 1 THEN '0' + CONVERT(NVARCHAR(3), @iMinutos)    
+												ELSE CONVERT(NVARCHAR(8), @iMinutos)      
+												END + ':00'
+			END
+	END
+		
+   IF @MINUTOS > 0
+	BEGIN
+	   SET @sEdita = CASE LEN(@iHoras)
+					 WHEN 0 THEN '00'
+					 WHEN 1 THEN '0' + CONVERT(NVARCHAR(1), @iHoras)
+					 ELSE CONVERT(NVARCHAR(8),@iHoras)
+					 END
+	
+	   SET @sEdita = @sEdita + ':' + CASE LEN(@iMinutos)
+									 WHEN 0 THEN '00' 
+									 WHEN 1 THEN '0' + CONVERT(NVARCHAR(3), @iMinutos)    
+									 ELSE CONVERT(NVARCHAR(8), @iMinutos)      
+									 END + ':00'
+	END
+
+
+
+   IF @sEdita = '00:00:00' BEGIN SET @sEdita = '     '  END
+    
+RETURN @sEdita
+END
